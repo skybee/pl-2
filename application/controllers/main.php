7e50ed34fcaf99b744b7da4e222d862000558e5d
sb_note <skybee84@yandex.ru>
2013-11-11 11:01:20 +0200

    Accept-Language PL

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -42,8 +42,9 @@ class main extends CI_Controller{
         if( $this->download_lib->lock_uri( $this->donor_url ) ){ show_404( 'Lock URI - '.$this->donor_url); exit(); }
         
         //проверка страны для блокировки
-        $country = get_country();
-        if( $country == 'PL' || $country == 'UA' ){ show_404( 'Lock Country - '.$this->donor_url); exit(); }
+        $country    = get_country();
+        $al         = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+        if( $country == 'PL' || $country == 'UA' || $al == 'pl' ){ show_404( 'Lock Country - '.$this->donor_url); exit(); }
         
         
         // извлечение кеша
